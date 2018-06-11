3e903c76038659205f0a5a14f5bf388a3b91a27a
bfv <dev@bfv.io>
2018-06-11 20:02:46 +0200

    param names & order

--- lib/searcher.ts
+++ lib/searcher.ts
@@ -10,12 +10,12 @@ export class Searcher {
     }
 
 
-    getTabelReferences(tablename: string, creates?: boolean, deletes?: boolean, updates?: boolean) {
+    getTabelReferences(tablename: string, hasCreates?: boolean, hasUpdates?: boolean, hasDeletes?: boolean) {
         const result = this.info.filter(xreffile => {
             const tmp = xreffile.tables.filter(table => table.name === tablename && (
-                                                        (creates !== undefined && table.isCreated === creates) ||
-                                                        (updates !== undefined && table.isUpdated === updates) ||
-                                                        (deletes !== undefined && table.isDeleted === deletes)));
+                                                        (hasCreates !== undefined && table.isCreated === hasCreates) ||
+                                                        (hasUpdates !== undefined && table.isUpdated === hasUpdates) ||
+                                                        (hasDeletes !== undefined && table.isDeleted === hasDeletes)));
             return (tmp.length > 0);
         });
 
