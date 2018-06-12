2e3dbff815d5acbeb5962485da25a0cda241d683
bfv <dev@bfv.io>
2018-06-12 13:09:07 +0200

    #3 fixed no results w/o params

--- lib/searcher.ts
+++ lib/searcher.ts
@@ -11,11 +11,15 @@ export class Searcher {
 
 
     getTabelReferences(tablename: string, hasCreates?: boolean, hasUpdates?: boolean, hasDeletes?: boolean) {
+
+        const noCriteria = (hasCreates === undefined && hasUpdates === undefined && hasDeletes === undefined);
+
         const result = this.info.filter(xreffile => {
             const tmp = xreffile.tables.filter(table => table.name === tablename && (
                                                         (hasCreates !== undefined && table.isCreated === hasCreates) ||
                                                         (hasUpdates !== undefined && table.isUpdated === hasUpdates) ||
-                                                        (hasDeletes !== undefined && table.isDeleted === hasDeletes)));
+                                                        (hasDeletes !== undefined && table.isDeleted === hasDeletes) ||
+                                                        (noCriteria)));
             return (tmp.length > 0);
         });
 
