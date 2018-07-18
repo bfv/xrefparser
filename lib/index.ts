bad6286def476fa0d136b9ac21b3b2e03e4ca5c6
bfv <dev@bfv.io>
2018-07-18 11:40:04 +0200

    exported ParserConfig

--- lib/index.ts
+++ lib/index.ts
@@ -1,5 +1,5 @@
 
-export { Parser } from './parser';
+export { Parser, ParserConfig } from './parser';
 export { XrefFile } from './xreffile';
 export { Class, Field, Table } from './model';
 export { Searcher } from './searcher';
